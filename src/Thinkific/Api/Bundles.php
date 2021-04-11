abfc1fc0461c9f0700c1aa08f2238978ec194ba7
Kyle Yee <shauleong@gmail.com>
2021-04-11 17:33:47 +0800

    remove unimplmented function

--- src/Thinkific/Api/Bundles.php
+++ src/Thinkific/Api/Bundles.php
@@ -10,39 +10,4 @@ class Bundles extends AbstractApi {
             "query"      => $data
         ] ), true );
     }
-
-    /**
-     * @throws ApiException
-     */
-    public function getAll() {
-        throw new ApiException( [ "Not Supported" => [ "The API Call 'getAll()' is not supported by the Thinkific Api" ] ] );
-    }
-
-    /**
-     * @throws ApiException
-     */
-    public function add() {
-        throw new ApiException( [ "Not Supported" => [ "The API Call 'getAll()' is not supported by the Thinkific Api" ] ] );
-    }
-
-    /**
-     * @throws ApiException
-     */
-    public function delete() {
-        throw new ApiException( [ "Not Supported" => [ "The API Call 'getAll()' is not supported by the Thinkific Api" ] ] );
-    }
-
-    /**
-     * @throws ApiException
-     */
-    public function getById() {
-        throw new ApiException( [ "Not Supported" => [ "The API Call 'getAll()' is not supported by the Thinkific Api" ] ] );
-    }
-
-    /**
-     * @throws ApiException
-     */
-    public function update() {
-        throw new ApiException( [ "Not Supported" => [ "The API Call 'getAll()' is not supported by the Thinkific Api" ] ] );
-    }
 }
